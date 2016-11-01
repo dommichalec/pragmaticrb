2fb814aee42ce404b29a0c5273ce051a5e39cf7e
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-11-01 18:59:54 -0400

    add game_turn module

--- studio_game/game.rb
+++ studio_game/game.rb
@@ -1,5 +1,5 @@
 require_relative 'player' # player.rb is relative to game in same directory
-require_relative 'die'
+require_relative 'game_turn' # game_turn.rb is relative to game in same directory
 
 class Game
   # attributes
@@ -20,16 +20,7 @@ class Game
   def play
     puts "There are #{@players.size} players in #{@title}:"
     @players.each do |player|
-      die = Die.new
-      number_rolled = die.roll
-      case number_rolled
-      when 1..2
-        player.blam!
-      when 3..4
-        puts "#{player.name} was skipped."
-      else
-        player.woot!
-      end
+      GameTurn.take_turn_for(player)
       puts player
     end
   end
---
+++ studio_game/game_turn.rb
@@ -0,0 +1,19 @@
+require_relative 'die'
+require_relative 'player'
+
+module GameTurn
+
+  # rolls die for randomness then returns whether player is blammed, wooted, or
+  # skipped
+  def self.take_turn_for(player)
+    die = Die.new
+    case die.roll
+    when 1..2
+      player.blam!
+    when 3..4
+      puts "#{player.name} was skipped."
+    else
+      player.woot!
+    end
+  end
+end
